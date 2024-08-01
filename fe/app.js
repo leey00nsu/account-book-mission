4db272e170e441429b75b82639b998beb8f06685
leeyoonsu <dbstndla1212@naver.com>
2024-08-01 16:05:40 +0900

    chore(#4) : fe 웹팩 설정, express 설정

--- fe/app.js
+++ fe/app.js
@@ -1,11 +1,26 @@
 const express = require('express');
-const app = express();
-const port = 3000;
+const path = require('path');
 
+const app = express();
+const PORT = 3001;
+
+// 정적 파일 제공
+app.use(express.static('public'));
+app.use('/dist', express.static('dist'));
+
+// 라우트 설정
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.get('/pageA', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'pageA.html'));
+});
+
+app.get('/pageB', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'pageB.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
